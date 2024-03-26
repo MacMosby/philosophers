9aee53425445f716ff6764d481f5adcf36638688
Marc Rodenbusch <marcrodenbusch@gmail.com>
2024-03-26 19:27:45 +0100

    basic testing to understand threads

---
+++ main.c
@@ -0,0 +1,34 @@
+#include <pthread.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+void	*routine()
+{
+	printf("test\n");
+	sleep(3);
+	printf("new line\n");
+	return (NULL);
+}
+
+int	main(int argc, char **argv)
+{
+	pthread_t	t1, t2;
+	if (pthread_create(&t1, NULL, &routine, NULL) != 0)
+	{
+		return (1);
+	}
+	if (pthread_create(&t2, NULL, &routine, NULL) != 0)
+	{
+		return (2);
+	}
+	if (pthread_join(t1, NULL) != 0) // second argument is a pointer that takes the return from the thread
+	{
+		return (3);
+	}
+	if (pthread_join(t2, NULL) != 0)
+	{
+		return (4);
+	}
+	return (0);
+}
